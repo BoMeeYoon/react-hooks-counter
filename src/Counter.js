c3117c6c8f5b9e1c5d4127d6b9c107be1414e401
bomee <primavera0331@gmail.com>
2020-05-09 20:01:23 +0900

    Initialize project using Create React App

---
+++ src/Counter.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function Counter () {
+
+
+    return (
+        <div>
+            <h1>0</h1>
+            <button>+</button>
+            <button>-</button>
+        </div>
+    )
+
+}
